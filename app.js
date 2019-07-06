57ab2522eb92f1e38041111262d82b4e378c49d2
archeel <archeelparekh11@gmail.com>
2019-07-06 15:54:12 +0530

    .

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
+var mongoose=require("mongoose");
 
+mongoose.connect("mongodb://localhost/clinicapp");
 app.use(express.static('pubic'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.set("view engine","ejs");
+
+var doctorSchema = new mongoose.Schema({
+	fname: String,
+	lname: String,
+	email: String,
+	password: String,
+	authenticationKey: String
+ });	
+
+var doctor = mongoose.model("doctor", doctorSchema);
 
 app.get("/",function(req,res){
 		res.render("homepage");
@@ -35,11 +47,24 @@ app.get("/logout",function(req,res){
 });
 
 app.post("/dsignup",function(req,res){
-	res.redirect("/signin");
-	console.log("a doctor has signed up");
-	console.log("name : "+req.body.fname);
-	console.log("password : "+req.body.password);
-	console.log("password : "+req.body.authenticationKey);
+	var fname = req.body.fname;
+    var lname = req.body.lname;
+	var email = req.body.email;
+	var password = req.body.password;
+	var authenticationKey = req.body.authenticationKey;
+    var newDoctor = {
+		fname: fname,
+		lname: lname,
+		email: email,
+		password: password,
+		authenticationKey: authenticationKey}
+    doctor.create(newDoctor, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/signin");
+        }
+    });
 });
 
 app.post("/psignup",function(req,res){
@@ -49,6 +74,16 @@ app.post("/psignup",function(req,res){
 	console.log("password : "+req.body.password);
 });
 
-app.listen(3000,function(){
-	console.log("Server For Clinic App Has Started");
+app.get("/doctors",function(req,res){
+	doctor.find({}, function(err, alldoctors){
+		if(err){
+			console.log(err);
+		} else {
+		   res.render("doctors",{doctors:alldoctors});
+		}
+	})
 });
+
+app.listen(3000, function(){
+	console.log("The Clinicapp Server Has Started!");
+ });
